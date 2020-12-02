fc2fa27b3d3df174dd6b17668de34f44dfd35133
Ricardo Masuda <m-ricardo1611@hotmail.com>
2020-12-01 22:33:14 -0300

    inicio

---
+++ TrabalhoEMC0101/TrabalhoEMC0101/TrabalhoEMC0101/ViewModels/MasterPageVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TrabalhoEMC0101.ViewModels
+{
+    class MasterPageVM
+    {
+        public int numeroBinario { get; set; }
+        public int numeroDecimal { get; set; }
+        public Command ConverterCMD { get; set; }
+        public  MasterPageVM()
+        {
+            ConverterCMD = new Command(Converter);
+        }
+
+        public void Converter()
+        {
+            
+        }
+    }
+}
